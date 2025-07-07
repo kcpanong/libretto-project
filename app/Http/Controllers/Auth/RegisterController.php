8dcff1cad164caf13735f20084034530fc89c246
kcpanong <kcpanong@gmail.com>
2025-07-07 15:21:58 +0800

    Implemented Sanctum

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function show() {
+        return view('auth.register');
+    }
+
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        auth()->login($user);
+
+        $plainToken = $user->createToken('libretto-token');
+        $plainToken->accessToken->expires_at = now()->addDay();
+        $plainToken->accessToken->save();
+
+        return redirect()->route('dashboard');
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Pagination\Paginator;
+use App\Http\Middleware\CheckTokenExpiry;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function configureMiddleware(Middleware $middleware): void
+    {
+        $middleware->append(CheckTokenExpiry::class); // Global web middleware
+    }
+
     /**
      * Register any application services.
      */
@@ -19,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrapFive();
     }
 }
